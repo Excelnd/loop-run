128a57f832b63ffd23e43e418ddbe4a6ffd24de6
ihsan <ihsanmpm@gmail.com>
2017-12-17 12:46:33 +0530

    Add project files.

---
+++ loop run/loop run.cpp
@@ -0,0 +1,18 @@
+// loop run.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 4;
+	do {
+		cout << num;
+	}
+	while (num < 0);
+
+
+}
+
